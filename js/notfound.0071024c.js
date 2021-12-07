688336f6571a3d60d6ea4e5ca3567a72f0c1ee17
xun0101 <90738788+xun0101@users.noreply.github.com>
2021-12-07 07:10:05 +0000

    Deploying to gh-pages from @ xun0101/shop@8f891ef6c7070a6fe128e72ae92bae0a1d62fc1a 🚀

---
+++ js/notfound.0071024c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["notfound"],{"2a31":function(t,n,e){"use strict";e.r(n);var o=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"text-center",attrs:{id:"notfound"}},[e("h1",[t._v("找不到")]),e("b-btn",{attrs:{size:"lg",variant:"primary"},on:{click:t.gohome}},[t._v("回首頁")])],1)},s=[],r={methods:{gohome:function(){this.$router.push("/")}}},a=r,i=e("2877"),u=Object(i["a"])(a,o,s,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=notfound.0071024c.js.map
